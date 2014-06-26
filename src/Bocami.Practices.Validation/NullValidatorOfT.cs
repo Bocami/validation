38780cf57a81db1b1b58f671c5668f700f07e654
Jonatan Pedersen <jp@jonatanpedersen.com>
2014-06-26 23:13:10 +0200

    Made NullValidatorOfT not sealed

--- src/Bocami.Practices.Validation/NullValidatorOfT.cs
+++ src/Bocami.Practices.Validation/NullValidatorOfT.cs
@@ -1,6 +1,6 @@
 ﻿namespace Bocami.Practices.Validation
 {
-    public sealed class NullValidator<T> : IValidator<T> 
+    public class NullValidator<T> : IValidator<T> 
         where T : class
     {
         public void Validate(T t)
